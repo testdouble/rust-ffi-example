1146777169d813a94c81c1ce73c62592875d4d6f
Schoonology <michael.r.schoonmaker@gmail.com>
2017-11-02 12:33:44 -0700

    Add ring to bell.

--- unity/Assets/ServiceBell.cs
+++ unity/Assets/ServiceBell.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ServiceBell : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				gameObject.GetComponent<AudioSource>().Play();
+			}
+		}
 	}
 }
