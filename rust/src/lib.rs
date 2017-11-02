67c077fb1509316306e72974b1308903d7a71229
Schoonology <michael.r.schoonmaker@gmail.com>
2017-11-02 12:44:14 -0700

    Add Rust project with bindings.

---
+++ rust/src/lib.rs
@@ -0,0 +1,106 @@
+use std::ffi::CStr;
+use std::mem::{drop, transmute};
+use std::net::UdpSocket;
+use std::os::raw::c_char;
+use std::ptr;
+use std::result::Result;
+
+pub struct Baton {
+    socket: UdpSocket,
+}
+
+impl<'a> Baton {
+    fn to_ptr(self) -> *mut Baton {
+        unsafe { transmute(Box::new(self)) }
+    }
+
+    fn from_ptr(ptr: *mut Baton) -> &'a mut Baton {
+        unsafe { &mut *ptr }
+    }
+
+    fn connect(url: &str) -> Result<Baton, String> {
+        println!("Connecting to {:}...", url);
+
+        let socket = match UdpSocket::bind("127.0.0.1:12345") {
+            Ok(socket) => socket,
+            Err(error) => return Err(format!("{:}", error)),
+        };
+
+        Ok(Baton { socket: socket })
+    }
+
+    fn disconnect(ptr: *mut *mut Baton) {
+        let baton: Box<Baton> = unsafe { transmute(*ptr) };
+
+        drop(baton);
+    }
+
+    fn send_ding(&mut self) -> Result<(), String> {
+        match self.socket.send_to(&[0; 10], "127.0.0.1:12346") {
+            Ok(_) => Ok(()),
+            Err(error) => Err(format!("{:}", error)),
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn connect_to_server(ptr: *mut *const Baton, url: *const c_char) -> bool {
+    let url = unsafe { CStr::from_ptr(url) };
+    let url_str = match url.to_str() {
+        Ok(slice) => slice,
+        Err(_) => {
+            println!("Invalid UTF8 in URL.");
+            return false;
+        }
+    };
+
+    match Baton::connect(url_str) {
+        Ok(baton) => {
+            println!("Connected.");
+
+            unsafe {
+                *ptr = baton.to_ptr();
+            }
+
+            true
+        }
+        Err(message) => {
+            println!("Failed to connect: {:}", message);
+
+            unsafe {
+                *ptr = ptr::null();
+            }
+
+            false
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn disconnect_from_server(ptr: *mut *mut Baton) {
+    if !ptr.is_null() && unsafe { !(*ptr).is_null() } {
+        Baton::disconnect(ptr);
+
+        println!("Disconnected.");
+
+        unsafe {
+            *ptr = ptr::null_mut();
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn send_ding(ptr: *mut Baton) -> bool {
+    if !ptr.is_null() {
+        match Baton::from_ptr(ptr).send_ding() {
+            Ok(_) => true,
+            Err(message) => {
+                println!("Error while sending: {:}", message);
+
+                false
+            }
+        }
+    } else {
+        false
+    }
+}
